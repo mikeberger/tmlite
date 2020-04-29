bf0f63d60904e4aa1afa10365b606b67238ebc23
Mike Berger <mike@mbcsoft.com>
2020-04-29 12:04:30 -0400

    java 13

--- TicketMavenRoot/tm_installer/buildjre.bat
+++ TicketMavenRoot/tm_installer/buildjre.bat
@@ -1,2 +1,2 @@
-set "JAVA_HOME=c:\jdk-12"
+set "JAVA_HOME=c:\java\jdk-13"
 %JAVA_HOME%\bin\jlink --module-path %JAVA_HOME%\jmods --add-modules java.desktop,java.prefs,java.sql,java.xml,java.logging --output target/jre
